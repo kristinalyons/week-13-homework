0c74afa4bea9a0d6e1023fc4d48a04a5de54d501
kristina.lyons <kristinavlyons@gmail.com>
2017-01-16 23:01:39 -0500

    update

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+
+    {
+      name: "Jerry",
+      photo: "http://devsite.webguidecms.ca/public/uploads/images/jerry.jpg",
+      scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "1",
+            "4",
+            "3",
+            "5",
+            "4",
+            "5"
+        ]
+    
+     },
+    {
+      name: "Elaine",
+      photo: "https://s-media-cache-ak0.pinimg.com/originals/52/89/d7/5289d7dd0c7c16f31df2b1c8037b048a.jpg",
+      scores: [
+            "4",
+            "5",
+            "1",
+            "3",
+            "3",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+       ]
+     },
+    {
+      name: "George",
+      photo: "https://content.linkedin.com/content/dam/business/marketing-solutions/global/en_US/blog/2015/11/george-costanza.jpg",
+      scores: [
+            "3",
+            "4",
+            "5",
+            "3",
+            "5",
+            "3",
+            "2",
+            "3",
+            "3",
+            "1"
+       ]
+     },
+    {
+      name: "Kramer",
+      photo: "https://pbs.twimg.com/profile_images/2211223948/original.jpg",
+      scores: [
+            "5",
+            "3",
+            "1",
+            "5",
+            "1",
+            "4",
+            "5",
+            "5",
+            "5",
+            "2"
+       ]
+     },
+    {
+      name: "Newman",
+      photo: "http://chronicle.augusta.com/sites/default/files/editorial/images/spotted/53/534466.jpg",
+      scores: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "3",
+            "2",
+            "3",
+            "3",
+            "1",
+            "5"
+       ]
+     },
+    {
+      name: "The Soup Nazi",
+      photo: "http://static.wixstatic.com/media/054162_2c3cf3d805603ce1ff4b93170394ea63.jpg_srz_160_195_85_22_0.50_1.20_0.00_jpg_srz",
+      scores: [
+            "1",
+            "2",
+            "3",
+            "5",
+            "5",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5"
+       ]
+     }
+];
+
+module.exports = friends;
